74218b0e36bd17259b67c16e904912188b6d0682
Jairo Atoche <jairoatoche90098@gmail.com>
2022-07-31 22:17:56 -0500

    add: cambiando componente funcional a componente de clase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import './App.css';
 import Testimonio from './componentes/Testimonio';
 
+/*** Componente funcional ***/
+/*
 function App() {
   let testimonios = {
     shawn: {
@@ -38,6 +41,50 @@ function App() {
       </div>
     </div>
   );
+}
+*/
+
+/*** Componente de clase ***/
+class App extends React.Component {
+  render() {
+    let testimonios = {
+      shawn: {
+        img:'shawn',
+        nombre:'Shawn Wang',
+        pais: 'Singapur',
+        cargo: 'Ingeniero de Software',
+        empresa: 'Amazon',
+        texto: 'Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+      },
+      sarah: {
+        img:'sarah',
+        nombre:'Sarah Chima',
+        pais: 'Nigeria',
+        cargo: 'Ingeniero de Software',
+        empresa: 'ChatDesk',
+        texto: 'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+      },
+      emma: {
+        img:'emma',
+        nombre:'Emma Bostian',
+        pais: 'Suecia',
+        cargo: 'Ingeniero de Software',
+        empresa: 'Spotify',
+        texto: 'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+      }    
+    };
+
+    return (
+      <div className="App">
+        <div className='main'>
+          <h1 className='main__h1'>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          <Testimonio imagen={testimonios.shawn.img} nombre={testimonios.shawn.nombre} pais={testimonios.shawn.pais} cargo={testimonios.shawn.cargo} empresa={testimonios.shawn.empresa} testimonio={testimonios.shawn.texto} />
+          <Testimonio imagen={testimonios.sarah.img} nombre={testimonios.sarah.nombre} pais={testimonios.sarah.pais} cargo={testimonios.sarah.cargo} empresa={testimonios.sarah.empresa} testimonio={testimonios.sarah.texto} />
+          <Testimonio imagen={testimonios.emma.img} nombre={testimonios.emma.nombre} pais={testimonios.emma.pais} cargo={testimonios.emma.cargo} empresa={testimonios.emma.empresa} testimonio={testimonios.emma.texto} />
+        </div>
+      </div>
+    );
+  }  
 }
 
 export default App;
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import  '../css/Testimonio.css';
 
+/*** Componente funcional ***/
+/*
 function Testimonio(props) {
   return(
     <div className='testimonio'>
@@ -12,6 +14,23 @@ function Testimonio(props) {
       </div>
     </div>
   );
+}
+*/
+
+/*** Componente de clase ***/
+class Testimonio extends React.Component {
+  render() {
+    return (
+      <div className='testimonio'>
+          <img className='testimonio__img' src={require(`../img/testimonio-${this.props.imagen}.png`)} alt={`Fotografia de ${this.props.imagen}`}/>
+        <div className='testimonio__contenido'>
+          <p className='testimonio__nombre'><strong>{this.props.nombre}</strong> en {this.props.pais}</p>
+          <p className='testimonio__cargo'>{this.props.cargo} en <strong>{this.props.empresa}</strong></p>
+          <p className='testimonio__texto'>"{this.props.testimonio}"</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Testimonio;
